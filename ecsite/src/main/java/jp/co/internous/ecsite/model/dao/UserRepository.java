a0319e00a0d3d331cf346d55f58deaf3b0d95715
k-kawamura-internous <k.kawamura@internous.co.jp>
2021-06-17 13:39:46 +0900

    確認

--- ecsite/src/main/java/jp/co/internous/ecsite/model/dao/UserRepository.java
+++ ecsite/src/main/java/jp/co/internous/ecsite/model/dao/UserRepository.java
@@ -3,12 +3,15 @@ package jp.co.internous.ecsite.model.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import jp.co.internous.ecsite.model.entity.User;
-
-
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	List<User> findByUserNameAndPassword(String userName, String password);
+	// usernameを探すために追加
+	@Query(value="SELECT count(id) FROM user WHERE user_name = :userName",
+			nativeQuery=true)
+	int findCountByUserName(@Param("userName") String userName);
+
 }
